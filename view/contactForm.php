4dff0047b4d8c5e8092dec98b3d403f97182c283
Jason Dark <jason@jasondarkx2.com>
2016-09-22 15:17:19 -0400

    Finshed off the ContactFormView file

--- view/contactForm.php
+++ view/contactForm.php
@@ -1,8 +1,17 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<div id="contactForm">
+<form  id="msg" name="msg" method="post" action="<?php echo the_permalink();?>">
+<label class="field"><span class="noti">*</span>Name:</label><input type="text" id="cname" name="cname" maxlength="75"  /><br/>
+<label class="field"><span class="noti">*</span>E-mail:</label><input type="text" id="email" name="email" maxlength="75" /><br/>
+<label id="field"><span class="noti">*</span>Subject:</label><input type="text" id="subject" name="subject" maxlength="75" /><br/>
+<span class="noti">*</span>Message:
+<br/><textarea id="message" name="message" rows="15" cols="70"></textarea>
+<br/>
+<label id="success" class="error">*<span style="color:white">Required fields</span> </label>
+<br/>
+<span class="sbuttons">
+<input type="submit" id="submit" value="Send Message"/>
+<input type="reset" id="clear" value="clear"/>
+</span>
+</form>
+</div>
+        
