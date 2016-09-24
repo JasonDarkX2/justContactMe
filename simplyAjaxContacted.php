65594815c5d03eee17623c4108e36137ce37264d
Jason Dark <jason@jasondarkx2.com>
2016-09-23 20:22:59 -0400

    Completed the plugin file with scripts and view functions

--- simplyAjaxContacted.php
+++ simplyAjaxContacted.php
@@ -1,8 +1,34 @@
 <?php
+/*
+Plugin Name: Simply AJAX Contacted
+Plugin URI: http://www.jasondarkx2.com
+Description: Simply an AJAX Contact form 
+Author:JasonDarkX2
+version: x.xx
+Author URI:http://www.jasondarkx2.com 
+*/ 
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+class SimplyAjaxContacted{
+    function init(){  
+        add_shortcode('AjaxContactForm',array(__CLASS__,'contactView'));
+    }
+    static  function add_scripts($hook){
+         wp_enqueue_style('sac-style', plugins_url('_inc/SimplyAjaxContacted.css', __FILE__));
+         wp_enqueue_script('sac-script', plugins_url('_inc/SimplyAjaxContacted.js', __FILE__), array('jquery'));
+         wp_enqueue_script('sac-script', 'http://ajax.aspnetcdn.com/ajax/jquery.validate/1.9/jquery.validate.min.js', array('jquery'));
+    }
+    
+
+    
+     function contactView(){
+      add_action('wp_footer', array(__CLASS__, 'add_scripts'));
+ob_start();
+     include (plugin_dir_path(__FILE__) .'/view/contactForm.php');
+     $output=ob_get_contents();;
+     ob_end_clean();
+     return $output;
+ }
+}
+ add_action( 'wp_loaded',SimplyAjaxContacted::init());
 
