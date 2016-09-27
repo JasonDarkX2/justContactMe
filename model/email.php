128349739d9fab74a4f34d4f1addab4e9b057ba8
Jason Dark <jason@jasondarkx2.com>
2016-09-26 20:36:00 -0400

    Completed the model class for plugin

--- model/email.php
+++ model/email.php
@@ -6,3 +6,40 @@
  * and open the template in the editor.
  */
 
+$url= explode('plugin',$_SERVER['SCRIPT_FILENAME']);
+require_once dirname($url[0])  . '/wp-load.php';
+Class Email{
+  static  $email;
+    function __construct() {
+      $email= array('to','from','subject', 'header','message'.'headers');
+           
+    }
+    
+    function createEmail($to,$name,$from,$subject,$message, $extraHeaders){
+        $headerIndex=array();
+        self::$email['to']=$to;
+      self::$email['from']="From:$name<$from>" . "\r\n";
+      array_push($headerIndex,self::$email['from']);
+      array_push($headerIndex,"Reply-To:<$from>" . "\r\n");
+      array_push($headerIndex, "Recived:from<$name>" . "\r\n");
+      self::$email['subject']=$subject;
+      self::$email['message']=$message;
+      
+      if(COUNT($extraHeaders)>0){
+      foreach($extraHeaders as $x){
+          array_push($headerIndex, $x);
+      }
+      }
+      self::$email['headers']=$headerIndex;
+      var_dump(self::$email);
+    }
+    
+    function sendmail(){
+        
+        $status=wp_mail( self::$email['to'], self::$email['subject'], self::$email['message'], self::$email['headers'], $attachments );
+        
+    return $status;
+        
+    }
+    
+}
