19b9579ed956cbb9f2313824beac16f9290e7b8a
Jason Dark <jason@jasondarkx2.com>
2016-09-26 20:37:50 -0400

    Completed the controller for plugin

--- controller/email_controller.php
+++ controller/email_controller.php
@@ -1,8 +1,20 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require_once dirname(dirname(__FILE__)) . '/model/email.php';
+$mail= new Email();
+$to=get_option('admin_email');
+$name =preg_replace('/[^A-Za-z0-9\-]/', '',$_POST['cname']);
+$email = $_POST['email'];
+$message = $_POST['message'];
+$subject=preg_replace('/[^A-Za-z0-9\-]/', '',$_POST['subject']);
+$headers=array('placeholder');
+$mail->createEmail($to,$name,$email,$subject,$message, $headers);
+if($mail->sendmail()){
+    echo "sent";
+}
+else{
+    echo "failed";
+}
+?>
+
 
