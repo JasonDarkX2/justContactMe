160615ca7cd6350fd75b2cca1ad2a5eefe1826d7
Jason Dark <jason@jasondarkx2.com>
2016-09-27 21:01:02 -0400

    Setup for the recaptcha option

--- view/contactForm.php
+++ view/contactForm.php
@@ -1,5 +1,5 @@
 <div id="contactForm">
-<form  id="msg" name="msg" method="post" action="<?php echo  the_permalink(); ?>">
+    <form  id="msg" name="msg" method="post" action="<?php echo the_permalink(); ?>">
 <label class="field"><span class="noti">*</span>Name:</label><input type="text" id="cname" name="cname" maxlength="75"  /><br/>
 <label class="field"><span class="noti">*</span>E-mail:</label><input type="text" id="email" name="email" maxlength="75" /><br/>
 <label id="field"><span class="noti">*</span>Subject:</label><input type="text" id="subject" name="subject" maxlength="75" /><br/>
@@ -9,7 +9,10 @@
 <label id="success" class="error">*<span style="color:white">Required fields</span> </label>
 <br/>
 <span class="sbuttons">
-<input type="submit" id="submit" value="Send Message"/>
+    <?php if(get_option('reCaptchaEnabled')): ?>
+    <div class="g-recaptcha" data-sitekey="<?php get_option('siteKey'); ?>"></div>
+<?php endif;?>
+    <input type="submit" id="submit" value="Send Message"/>
 <input type="reset" id="clear" value="clear"/>
 </span>
 </form>
