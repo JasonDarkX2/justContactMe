543506d6bdaab1027f44858e887c2e20ed5f8534
Jason Dark <jason@jasondarkx2.com>
2016-09-30 18:41:55 -0400

    Implemented model functions handling recaptcha setting

---
+++ model/admin/settings.php
@@ -0,0 +1,25 @@
+<?php
+
+$url= explode('plugin',$_SERVER['SCRIPT_FILENAME']);
+require_once dirname($url[0])  . '/wp-load.php';
+class AdminSettings{
+    function __construct() {
+        
+    }
+    function setReCatcha($switch, $keys){
+        if($switch=='true'){
+            update_option('reCaptchaEnabled',TRUE);
+            foreach($keys as $key=> $value){
+                self::setReCaptchaKeys($key,$value);
+            }
+            
+        }else{
+            update_option('reCaptchaEnabled',FALSE);
+        }
+        
+    }
+    function setReCaptchaKeys($keyType, $value){
+        if(get_option('reCaptchaEnabled'))
+        update_option($keyType, $value); 
+    }
+}
