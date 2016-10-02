a36e1e2fec9bdb20d8b53e8ad7c17e3e94ee9199
Jason Dark <jason@jasondarkx2.com>
2016-10-01 21:47:22 -0400

    Added a reCaptchaConfig settings

--- view/admin/adminPage.php
+++ view/admin/adminPage.php
@@ -13,7 +13,35 @@
                 <input type="text" name="secretKey" value="<?php echo get_option('secretKey'); ?>"/>
                 <p><i>Need Keys</i><div class="toolTip"> <a href="#">[?]</a><span class="toolTipText">placeholder</span></div>
                 </p>
+                <?php foreach(get_option('reCaptchaConfig') as $i => $v): ?>
+                <?php switch($i){
+                case "theme": ?>
+                 <label for="theme"> reCaptcha Theme:</label>
+                <input type='hidden' name='confId[]' value="<?php echo $i; ?>"  id="configId"> 
+                <input name="<?php echo $i; ?>" type="radio" value="light" <?php checked($v, 'light');?>>Light
+                    <input  name="<?php echo $i; ?>" type="radio" value="dark" <?php  checked($v, 'dark');?> >Dark
+                    <br/>
+                    <?php
+                    break;
+                case 'type':?>
+                    <label for="theme"> reCaptcha Type:</label>
+                <input type='hidden' name='confId[]' value="<?php echo $i; ?>"  id="configId"> 
+                    <input name="<?php echo $i; ?>" type="radio" value="image" <?php  checked($v, 'image');?>>Image
+                    <input  name="<?php echo $i; ?>" type="radio" value="audio" <?php  checked($v, 'audio');?>>Audio
+                    <br/>
+                <?php break;
+                case "size":?>
+                <label for="theme"> reCaptcha Size:</label>
+                <input type='hidden' name='confId[]' value="<?php echo $i; ?>"  id="configId"> 
+                    <input name="<?php echo $i; ?>" type="radio" value="normal"<?php  checked($v, 'normal');?> >Normal
+                    <input  name="<?php echo $i; ?>" type="radio" value="compact" <?php  checked($v, 'compact');?>>Compact
+                    <br/>
+                    <?php break;
+                }
+                ?>
+                    <?php endforeach;?>
                 </div>
+                  
                 <input type="submit" value="Save"/>
           </form>
 </div>
