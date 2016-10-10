1dd5394ccfbd453c9f2e2c58dbde684c9177d130
Jason Dark <jason@jasondarkx2.com>
2016-10-09 20:33:29 -0400

    CSS styling contact form

--- view/contactForm.php
+++ view/contactForm.php
@@ -1,13 +1,11 @@
 <div id="contactForm">
     <form  id="msg" name="msg" method="post" action="<?php echo the_permalink(); ?>">
-<label class="field"><span class="noti">*</span>Name:</label><input type="text" id="cname" name="cname" maxlength="75"  /><br/>
-<label class="field"><span class="noti">*</span>E-mail:</label><input type="text" id="email" name="email" maxlength="75" /><br/>
-<label id="field"><span class="noti">*</span>Subject:</label><input type="text" id="subject" name="subject" maxlength="75" /><br/>
-<span class="noti">*</span>Message:
-<br/><textarea id="message" name="message" rows="15" cols="70"></textarea>
-<br/>
+<label class="field"><span class="noti">*</span>Name:</label><input type="text" id="cname" name="cname" class="ifield" maxlength="75"  /><br/>
+<label class="field"><span class="noti">*</span>E-mail:</label><input type="text" id="email" name="email" class="ifield" maxlength="75" /><br/>
+<label id="field"><span class="noti">*</span>Subject:</label><input type="text" id="subject" name="subject" class="ifield" maxlength="75" /><br/>
+<label class="field"><span class="noti">*</span>Message:</label>
+<br/><textarea id="message" class="ifield" name="message" rows="15" cols="250"></textarea>
 <div id="success" ><span class="noti">*</span><span style="color:white">Required fields</span> </div>
-<br/>
 <span class="sbuttons">
     <?php if(get_option('reCaptchaEnabled')==TRUE): ?>
     <div class="g-recaptcha" data-sitekey="<?php echo get_option('siteKey'); ?>"
