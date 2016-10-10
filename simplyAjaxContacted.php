5100c0262923b74cb3e67acec7bdc5270fefe815
Jason Dark <jason@jasondarkx2.com>
2016-10-10 08:47:34 -0400

    Added Minified css/js files for contactForm

--- simplyAjaxContacted.php
+++ simplyAjaxContacted.php
@@ -48,6 +48,14 @@ class SimplyAjaxContacted{
          wp_enqueue_script('sac-script', plugins_url('_inc/SimplyAjaxContacted.js', __FILE__));
          wp_localize_script('sac-script', 'controller', $controllers);
     }
+        static  function addMinifiedScripts($hook){
+         wp_enqueue_style('sac-style', plugins_url('_inc/min/SimplyAjaxContacted.min.css', __FILE__));
+         wp_enqueue_script('sac-validate', 'http://ajax.aspnetcdn.com/ajax/jquery.validate/1.9/jquery.validate.min.js', array('jquery'));
+         wp_enqueue_script('sac-reCaptcha', 'https://www.google.com/recaptcha/api.js', array('jquery'));
+         $controllers = array('emailController' => plugins_url('controller/email_controller.php',__FILE__ ));
+         wp_enqueue_script('sac-script', plugins_url('_inc/min/SimplyAjaxContacted.min.js', __FILE__));
+         wp_localize_script('sac-script', 'controller', $controllers);
+    }
      static  function addAdminScripts($hook){
          wp_enqueue_style('sacAdmin-style', plugins_url('_inc/admin/sacAdmin.css', __FILE__));
          $controllers = array('settingsController' => plugins_url('controller/admin/settings_controller.php',__FILE__ ));
@@ -59,7 +67,7 @@ class SimplyAjaxContacted{
          include (plugin_dir_path(__FILE__) .'view/admin/adminPage.php');
      }
   function contactFormView(){
-      add_action('wp_footer', array(__CLASS__, 'add_scripts'));
+      add_action('wp_footer', array(__CLASS__, 'addMinifiedScripts'));
 ob_start();
      include (plugin_dir_path(__FILE__). '/view/contactForm.php');
      $output=ob_get_contents();;
