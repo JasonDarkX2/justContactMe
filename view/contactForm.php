68406e2e4cad8512a431984dffc78576e668e4d2
Jason Dark <jason@jasondarkx2.com>
2016-11-10 20:10:20 -0500

    Added optional attachment field to contactForm

--- view/contactForm.php
+++ view/contactForm.php
@@ -3,6 +3,10 @@
 <label class="field"><span class="noti">*</span>Name:</label><input type="text" id="cname" name="cname" class="ifield" maxlength="75"  /><br/>
 <label class="field"><span class="noti">*</span>E-mail:</label><input type="text" id="email" name="email" class="ifield" maxlength="75" /><br/>
 <label id="field"><span class="noti">*</span>Subject:</label><input type="text" id="subject" name="subject" class="ifield" maxlength="75" /><br/>
+<?php if(get_option('attachment')): ?>
+<label class="field">Attachment:</label>
+ <input id="sacAttachment" type = "text" name= "Attachment"  value="Choose File" class="ifield" >
+<?php endif;?>
 <label class="field"><span class="noti">*</span>Message:</label>
 <br/><textarea id="message" class="ifield" name="message" rows="15" cols="250"></textarea>
 <div id="success" ><span class="noti">*</span><span style="color:white">Required fields</span> </div>
@@ -19,6 +23,9 @@
     <input type="submit" id="submit" value="Send Message"/>
 <input type="reset" id="clear" value="clear"/>
 </span>
+<?php if(get_option('attachment')): ?>
+<input id = "file" type="file" style='visibility: hidden;' name="img" onchange="ChangeText(this, 'sacAttachment');"/>
+<?php endif;?>
 </form>
 </div>
         
