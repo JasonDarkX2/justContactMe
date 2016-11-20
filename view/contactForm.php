6001ff61e42718058d661c3aaf71b59dde4f51aa
Jason Dark <jason@jasondarkx2.com>
2016-11-19 22:09:54 -0500

    Made adjustment to contactForm

--- view/contactForm.php
+++ view/contactForm.php
@@ -1,15 +1,19 @@
-<div id="contactForm">
-    <form  id="msg" name="msg" method="post" action="<?php echo the_permalink(); ?>">
-<label class="field"><span class="noti">*</span>Name:</label><input type="text" id="cname" name="cname" class="ifield" maxlength="75"  /><br/>
-<label class="field"><span class="noti">*</span>E-mail:</label><input type="text" id="email" name="email" class="ifield" maxlength="75" /><br/>
-<label id="field"><span class="noti">*</span>Subject:</label><input type="text" id="subject" name="subject" class="ifield" maxlength="75" /><br/>
+<div id="contactSection">
+    <form  id="contactForm" method="post" action="<?php echo the_permalink(); ?>">
+<label class="field"><span class="noti">*</span>Name:</label><input type="text"  name="cname" class="ifield" maxlength="75"  /><br/>
+<label class="field"><span class="noti">*</span>E-mail:</label><input type="text"  name="email" class="ifield" maxlength="75" /><br/>
+<label id="field"><span class="noti">*</span>Subject:</label><input type="text" name="subject" class="ifield" maxlength="75" /><br/>
 <?php if(get_option('attachment')): ?>
 <label class="field">Attachment:</label>
- <input id="sacAttachment" type = "text" name= "mailAttachment"  value="<?php echo  inimsg; ?>" class="ifield" >
+ <input id="sacAttachment" type = "text" name="mailAttachment"  value="<?php echo  inimsg; ?>" class="ifield" >
 <?php endif;?>
+
 <label class="field"><span class="noti">*</span>Message:</label>
 <br/><textarea id="message" class="ifield" name="message" rows="15" cols="250"></textarea>
 <div id="success" ><span class="noti">*</span><span style="color:white">Required fields</span> </div>
+    <?php if(get_option('attachment')): ?>
+<input id = "file" type="file"  style="display:none;" name="sacfile" onchange="ChangeText(this, 'sacAttachment');"/>
+<?php endif;?>
 <span class="sbuttons">
     <?php if(get_option('reCaptchaEnabled')==TRUE): ?>
     <div class="g-recaptcha" data-sitekey="<?php echo get_option('siteKey'); ?>"
@@ -23,9 +27,7 @@
     <input type="submit" id="submit" value="Send Message"/>
 <input type="reset" id="clear" value="clear"/>
 </span>
-<?php if(get_option('attachment')): ?>
-<input id = "file" type="file" style='visibility: hidden;' name="img" onchange="ChangeText(this, 'sacAttachment');"/>
-<?php endif;?>
 </form>
+    
 </div>
         
