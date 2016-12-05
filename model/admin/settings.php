2fe689843e71383f763fd5386369d3c7dfdef0fd
Jason Dark <jason@jasondarkx2.com>
2016-12-05 08:56:29 -0500

    added getFormPreview

--- model/admin/settings.php
+++ model/admin/settings.php
@@ -87,7 +87,14 @@ class AdminSettings {
                 break;
         }
     }
-
+function getFormPreview(){
+    ob_start();
+       include (plugin_dir_path(dirname(dirname(__FILE__))) . 'view/contactForm.php');
+       $output = ob_get_contents();
+        ob_end_clean();
+       echo strip_tags($output,'<label><br><input><span><textarea><div>');
+}
+    
     function setReCatcha($switch, $keys) {
         if ($switch == 'true') {
             update_option('reCaptchaEnabled', TRUE);
