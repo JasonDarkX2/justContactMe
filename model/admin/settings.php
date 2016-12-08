0ec2051d562b2b3e8da4e4a69af1c52635859a10
Jason Dark <jason@jasondarkx2.com>
2016-12-07 20:12:27 -0500

    quick slashes fix

--- model/admin/settings.php
+++ model/admin/settings.php
@@ -27,6 +27,7 @@ class AdminSettings {
     function setCustomCSS($CSS) {
         $CSSFile = plugin_dir_path(dirname(dirname(__FILE__))) . '_inc/simplyAjaxContacted.css';
         $fileContent = file_get_contents($CSSFile);
+        $CSS=stripslashes($CSS);
         $content = "\r\n/*CustomCSS*/\r\n" . $CSS . "\r\n/*end CustomCSS*/";
         $fileContent = preg_replace("/\/\*CustomCSS\*\/(.*)end CustomCSS\*\//s", ' ', $fileContent) . $content;
         file_put_contents($CSSFile, $fileContent);
