40f5660a5a2e2c728754e1e502683c654f551cde
Jason Dark <jason@jasondarkx2.com>
2016-12-29 22:11:57 -0500

    setup uninstall script

--- uninstall.php
+++ uninstall.php
@@ -1,8 +1,10 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+if (
+        !defined('WP_UNINSTALL_PLUGIN') ||
+        !WP_UNINSTALL_PLUGIN ||
+        dirname(WP_UNINSTALL_PLUGIN) != dirname(plugin_basename(__FILE__))
+) {
+    status_header(404);
+    exit;
+}
