5ba2f2f58dd0c4b9cdc06c93ebff015e492fd593
Jason Dark <jason@jasondarkx2.com>
2017-01-02 20:35:23 -0500

    adjustments to adminpage UI

--- view/admin/adminPage.php
+++ view/admin/adminPage.php
@@ -141,6 +141,7 @@
                   <!--Contact form  section!-->
          <div id="ContactSetting" class="panelSection">
              <h1>ContactForm Settings</h1>
+             <div class="halfPanel">
              <label for="formTheme">themes<div class="toolTip">[?]<span class="toolTipText">Set the preferred contact Form theme.<strong> Default:</strong> &nbsp;None</span></div>:</label>
              <select name="formTheme">
                  <?php
@@ -163,13 +164,17 @@
                      <span class="toolTipText">Use this field for your custom Css styling for the contact Form.</span></div>:</label>
              <br/>
              <textarea  name="customCSS" rows="20"  cols="50"><?php  echo get_option('customCSS');?></textarea>
-         </div>
-                               <div  style=" margin-left:60px; border: solid 1px; display:inline-block;width:60%;">
+             </div>
+             <div class="halfPanel">
+                                              <div class="previewBox">
                  <strong>Contact form Preview</strong>
                   <?php
                   self::$settings->getFormPreview();
                   ?>
                   </div>
+             </div>
+         </div>
+  
          <!--end of ContactForm section!-->
          <div class="controlSection">
                      <input type="hidden" name="pluginDir" value ="<?php echo MY_PLUGIN_PATH; ?>"/>
