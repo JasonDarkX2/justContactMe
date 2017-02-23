0f9bda5a7cd360dfff930f55ed2da719a9e7d1a4
Jason Dark <jason@jasondarkx2.com>
2017-02-22 22:10:08 -0500

    added multiselect fields and control inputs

--- view/admin/tabs/logSettings.php
+++ view/admin/tabs/logSettings.php
@@ -7,9 +7,24 @@
  */
 ?>
 <h1>Mail Log and Blacklist</h1>
+<div class="log">
  <div class="halfPanel">
      <label> Mail Log</label>
+     <select name="whitelist" multiple>
+         <option value="Johndoe@doe.com">Johndoe@company.com</option>
+         <option value="bobred@doe.com">bobred@company.com</option>
+         </select>
  </div>
+    <div class="multicontrols">
+        <input type="button" id="logLeftBtn" value="&lt;&lt;" />
+        <br/>
+        <input type="button" id="logRightBtn" value="&gt;&gt;" />
+    </div>
  <div class="halfPanel">
      <label> BlackList</label>
+       <select name="blacklisted" multiple>
+         <option value="Johndoe@blocked.com">Johndoe@blocked.com</option>
+         <option value="bobred@bl;ocked.com">bobred@clocked.com</option>
+         </select>
  </div>
+</div>
