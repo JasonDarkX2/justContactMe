f26a17a5bd6494a1c0b337ae830fb7e504cda87e
Jason Dark <jason@jasondarkx2.com>
2017-03-14 21:11:55 -0400

    adjusted log UI

--- view/admin/tabs/logSettings.php
+++ view/admin/tabs/logSettings.php
@@ -15,6 +15,23 @@
            <option value="<?php echo $wl; ?>"> <?php echo $wl; ?></option>
           <?php } ?>
          </select>
+     <br/>
+            <label>Mail Log:</label>
+        <table border="1px" style="width:100%;">
+            <tr>
+            <th>date</th>
+            <th> Sender</th>
+            <th>Status</th>
+            <th>Options</th>
+            </tr>
+            <tr>
+                <td> March, 10,2017</td>
+                <td>Johdoe@domain.com</td>
+                <td> Send Fail</td>
+                <td>More info</td>
+            </tr>
+        </table>
+
  </div>
     <div class="multicontrols">
         <input type="button" id="logLeftBtn" value="&lt;&lt;" />
@@ -28,11 +45,8 @@
            <option value="<?php echo $bl; ?>"> <?php echo $bl; ?></option>
           <?php } ?>
          </select>
- </div>
- </div>
-<div class="log">
-<div class="halfPanel">
-            <label>error Log:</label>
+     <br/>
+     <label>error Log:</label>
         <table border="1px" style="width:100%;">
             <tr>
             <th>date</th>
@@ -47,24 +61,7 @@
                 <td>More info</td>
             </tr>
         </table>
-    </div>
 
-    <div class="halfPanel">
-        <label>error Log:</label>
-        <table border="1px" style="width:100%;">
-            <tr>
-            <th>date</th>
-            <th> Sender</th>
-            <th>Status</th>
-            <th>Options</th>
-            </tr>
-            <tr>
-                <td> March, 10,2017</td>
-                <td>Johdoe@domain.com</td>
-                <td> Send Fail</td>
-                <td>More info</td>
-            </tr>
-        </table>
-    </div>
- 
-</div>
+ </div>
+ </div>
+
